27907affc4dbee56e9b613be901e2e7b9ae0269e
David Joslin <david.joslin@ku.edu>
2025-11-08 16:26:30 -0600

    finish lab02

--- lab02/src/main.rs
+++ lab02/src/main.rs
@@ -1,20 +1,48 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
-    println!("How many points is your team favored by?");
+    let mut points = String::new();
     
-    let mut user_input = String::new();
+    print!("How many points is your team favored by?: ");
+    io::stdout().flush().unwrap();
 
     io::stdin()
-        .read_line(&mut user_input)
+        .read_line(&mut points)
         .expect("Failed to read line");
-
-    // println!("You Entered: {}", user_input.trim());
     
-    let favored_points: i32 = user_input
-        .trim()
-        .parse()
-        .expect("Failed to convert to i32");
+    let points = points.trim().parse::<f64>().expect("Couldn't parse points");
 
-    println!("Parsed number: {}", favored_points);
+    print!("Is it a home game? (h/H): ");
+    io::stdout().flush().unwrap();
+
+    let mut home_input = String::new();
+    io:: stdin()
+        .read_line(&mut home_input)
+        .expect("Failed to read line");
+    
+    let home_game = home_input.trim().eq_ignore_ascii_case("h");
+
+    print!("Are you picking the Bengals? (y/Y): ");
+    io::stdout().flush().unwrap();
+
+    let mut bengals_input = String::new();
+    io:: stdin()
+        .read_line(&mut bengals_input)
+        .expect("Failed to read line");
+    
+    let picked_bengals = bengals_input.trim().eq_ignore_ascii_case("y");
+    if picked_bengals {
+        println!("NO! NO! NO!");
+    } else if points >= 7.5 {
+        println!("SLAM IT!");
+    } else if (points >= 3.0) && (home_game) {
+        println!("Good bet");
+    } else if points >= 3.0 {
+        println!("Okay bet");
+    } else if (points > 0.0) && (home_game) {
+        println!("Highly risky");
+    } else {
+        println!("Don't bet!")
+    }
+
 }
